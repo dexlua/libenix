998da1be5ff373a4da889518843091641001fc80
dexlua <mateoandradeloyola.l@gmail.com>
2026-06-17 06:36:30 -0400

    update

--- Libenix_Web/app.py
+++ Libenix_Web/app.py
@@ -454,8 +454,8 @@ def detalle_estudiante(evaluacion_id, estudiante):
 # EJECUTAR
 # ==========================
 
-if __name__ == "__main__":
+import os
 
-    app.run(
-        debug=True
-    )
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
